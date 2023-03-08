88f04aa0eb4ea312d614260e028db6eef94deaf8
GabrielLima2803 <gabriellima2803@gmail.com>
2023-03-08 15:05:35 -0300

    Botar o professor no git.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <div class="mt-4 p-5 bg-primary text-white rounded">
+    <h1>Jumbotron Example</h1>
+    <p>Lorem ipsum...</p>
+    </div>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
